25a3e134f7b567d5ec8ec3b4dff2e210ee3f18f6
unknown <mdadib300@gmail.com>
2025-11-20 12:50:36 +0600

    Day 1

---
+++ main.py
@@ -0,0 +1,35 @@
+# This is Python Comment
+
+# Start printing something
+print("Hello World!")
+
+# Getting started with variables 
+# String
+my_name = "Md. Adib Chowdhury"
+print(my_name)
+# Wanna play with print?
+print(f"Bissash koren bhai, ami {my_name}")
+# Integer
+myAge = 21
+print(f"My age is {myAge}")
+# Float
+myHeight = 5.5
+print(f"My Height is not {myHeight}")
+# Boolean 
+isStudent = False
+print(f"He is {isStudent}, he is not a student.")
+
+# Conditional Statement (if else)
+isRobber = False
+if isRobber : print("He is robber")
+else : print("He is not robber")
+
+# Find the type of a variable [type(variableName)]
+isTrue = False
+print(type(isTrue))
+
+# Change the type of a variable [typeOfToChanged(variableName)] [str(), int(), float(), bool()]
+marks = 45
+print(type(marks))
+print(str(marks))
+print(type(str(marks)))
